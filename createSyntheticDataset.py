e9593203c50c3651021c6156c24e59d8e5fc173f
Alejandro Sanchez Ferrer <asanc.tech@gmail.com>
2022-08-17 01:07:50 +0200

    Update: Implemented mask pasting and area calculation - In process

--- createSyntheticDataset.py
+++ createSyntheticDataset.py
@@ -1,4 +1,5 @@
 import os
+from pickletools import uint8
 from sklearn import datasets
 from tqdm import tqdm
 import numpy as np
@@ -21,6 +22,7 @@ random.seed(1)
 ROT = True
 SCALE = True
 TRANS = True
+TRY = 3
 
 AUG_PATH = "../synthetic_dataset"
 
@@ -154,58 +156,88 @@ def pastePNG(bg, patch, pos = [0,0]):
     maskBGRInv = cv2.bitwise_not(maskBGR)
     imgMaskFull2[pos[1]:hp + pos[1], pos[0]:wp + pos[0], :] = maskBGRInv
 
-    bg = cv2.bitwise_and(bg, imgMaskFull2)
-    bg = cv2.bitwise_or(bg, imgMaskFull)
+    # Make a copy to avoid overwritting
+    bg_copy = bg.copy()
+    bg_copy = cv2.bitwise_and(bg_copy, imgMaskFull2)
+    bg_copy = cv2.bitwise_or(bg_copy, imgMaskFull)
 
-    return bg
+    return bg_copy
 
 #------------------------------------------------------------------------------
-def patch_img(background, patch):
+def pasteBinPNG(bg, patch, pos = [0,0]):
+    hp, wp, dp = patch.shape
+    hb, wb, db= bg.shape
+    
+    patchGray = cv2.cvtColor(patch, cv2.COLOR_BGR2GRAY)	
+    (thresh, patchBW) = cv2.threshold(patchGray, 0, 255, cv2.THRESH_BINARY)
+    patchBGR = cv2.cvtColor(patchBW, cv2.COLOR_GRAY2BGR)
+    imgMaskFull = np.zeros((hb,wb,db), np.uint8)
+    imgMaskFull[pos[1]:hp + pos[1], pos[0]:wp + pos[0],:] = patchBGR
+    cv2.imshow("maskfull",imgMaskFull)
+    cv2.waitKey(0)
+    # Make a copy to avoid overwritting
+    bg_copy = bg
+    cv2.imshow("bg",bg)
+    cv2.waitKey(0)
+    bg_copy = bg_copy + imgMaskFull
+    cv2.imshow("Sum",bg_copy)
+    cv2.waitKey(0)
+    return bg_copy
+
+#------------------------------------------------------------------------------
+def patch_img(bg_bin,background, patch,check = False):
     """
     Creates a synthetic using a background image an a png image as a patch
     """
-    p=patch
+    p= patch
     p_h, p_w, _ = p.shape
-    h, w, _ = background.shape
+    h, w, d = background.shape
     #print(p.shape)
-
-    if ROT:             #Apply random rotation and store it
-        # random.seed(1)
-        angle = randrange(360)
-        print(f"Applying rotation of {angle}")
-        p = ndimage.rotate(p, angle)
-    else:
-        angle = 0
-
-    if SCALE:               #Norm Rotated mask
-        while True:
+    paste = True
+    for i in range(TRY):
+        if ROT:             #Apply random rotation and store it
             # random.seed(1)
-            rs = 1
-            if max(h,w)/max(p_h,p_w) < 1: rs = max(h,w)/max(p_h,p_w)
-            rs *= float( randrange(25,50) ) / 100.0
-            p = cv2.resize(p,(0,0),None,fx=rs,fy=rs,interpolation=cv2.INTER_AREA)
-            p_h, p_w, _ = p.shape
-            if p_h < h and p_w < w:
+            angle = randrange(360)
+            print(f"Applying rotation of {angle}")
+            p = ndimage.rotate(p, angle)
+        else: angle = 0
+
+        if SCALE:               #Norm Rotated mask
+            while True:
+                # random.seed(1)
+                rs = 1
+                if max(h,w)/max(p_h,p_w) < 1: rs = max(h,w)/max(p_h,p_w)
+                rs *= float( randrange(25,50) ) / 100.0
+                p = cv2.resize(p,(0,0),None,fx=rs,fy=rs,interpolation=cv2.INTER_AREA)
+                p_h, p_w, _ = p.shape
                 print(f"Applying resize: {rs}")
-                #cv2.imshow("p", p)
-                #cv2.waitKey(0)
+                if p_h < h and p_w < w:
+                    break
+                    
+        else: rs = 1.0
+
+        if TRANS:               #Posicion en X/Y
+            p_h, p_w, _ = p.shape
+            # random.seed(1)
+            x = randrange(w - p_w)
+            # random.seed(1)
+            y = randrange(h - p_h)
+            print(f"Applying paste in location:{x,y}")
+
+        else: x,y = 0,0
+
+        if check:
+            bin_seg = pasteBinPNG(bg_bin, p, [x,y])
+            paste = checkOverlap(bg_bin,bin_seg)
+            if paste: 
+                bg_bin = bin_seg
                 break
-    else:
-        rs = 1.0
 
-    if TRANS:               #Posicion en X/Y
-        p_h, p_w, _ = p.shape
-        # random.seed(1)
-        x = randrange(w - p_w)
-        # random.seed(1)
-        y = randrange(h - p_h)
-        print(f"Applying paste in location:{x,y}")
-    else:
-        x,y = 0,0
+    if paste:
+        seg = pastePNG(background, p, [x,y])
+        return bin_seg,seg,angle,x,y,rs,True
+    else: return _,_,_,_,_,_,False
 
-    seg = pastePNG(background, p, [x,y])
-
-    return seg,angle,x,y,rs
 #------------------------------------------------------------------------------
 def obj_selector(bg_dataset,objects_dataset):
     objects, labels = obj_dataloader(objects_dataset)
@@ -221,116 +253,70 @@ def obj_selector(bg_dataset,objects_dataset):
             # Retrieving object to introduce:
             index = randrange(0,len(objects))
             objs.append((objects[index],labels[index]))
+        #Create hash table for every bg image and its objects associated
         bg_objs[n] = objs
         n+=1
-    return bg_objs
+    return bgs,bg_objs
+
 #------------------------------------------------------------------------------
 def create_new_dataset(bg_dataset, objects_dataset):
-    bg_objs = obj_selector(bg_dataset,objects_dataset)
-    for bg in tqdm((bg_objs), "Creating Dataset"):
-        for objs in bg_objs[bg]:
+    bgs,bg_objs = obj_selector(bg_dataset,objects_dataset)
+    n=0
+    # Iterate through bg images
+    for bg_idx in tqdm((bg_objs), "Creating Dataset"):
+        print(f"BG INDEX: {bg_idx}")
+        bg = bgs[bg_idx]
+        nimg_name = f"image_{n}" + ".jpg"
+
+        # Iterate through objects
+        i = 0
+        for objs in bg_objs[bg_idx]:
+            img_obj = objs[0]
             label = objs[1]
-            # 1. Parse JSON and extract points (TODO)
-            # 2. Create Polygons / Binary mask for each label (TODO)
-            # 3. Check overlapping between each one of them (TODO)
-            # 4. Get array of indices of good masks (TODO)
+
+            # 1. Calcular solape con n intentos
+            h,w,d = bg.shape
+            if i == 0 : bin_bg = np.zeros((h,w,d), np.uint8)
             
-        #extractMask(bg)
+
+            bin_bg,bg,rotation,x,y,rs,patched = patch_img(bin_bg,bg, img_obj,True)
+            if not patched:
+                continue
+            else:
+                cv2.imwrite(os.path.join(AUG_PATH,"images",nimg_name),bg)
+            
+            # 2. Crear json // Appendear al json (TODO)
+            patch_json(nimg_name,label,rotation,x,y,rs,img_obj,bg,os.path.join(AUG_PATH,"images",nimg_name))
+            i+=1
+        
+        n +=1
 
 #------------------------------------------------------------------------------
-def extractMask(bg):
-    # For each bg image
-    for obj,label in bg:
-        # Extract array of pairs and iterate through 
-        print(obj,label)
-        '''
-        data = json.load( open(json_path) )
-        assert len(data["shapes"]) != 0, f"Error: no shape in {json_path}"
+def checkOverlap(bin_img,obj_bin_bg):
+    overlap = bin_img + obj_bin_bg
+    cv2.imwrite("clean_bg.png",bin_img)
+    cv2.imwrite("with_obj_bg.png", obj_bin_bg)
+    cv2.imwrite("overlap.png", overlap)
+    
+    cv2.imshow("Overlap",overlap)
+    cv2.waitKey(0)
+    
+    overlapGRAY = cv2.cvtColor(overlap, cv2.COLOR_BGR2GRAY)
+    (thresh, overlapBW) = cv2.threshold(overlapGRAY, 127, 255, cv2.THRESH_BINARY)
+    contours,hierachy = cv2.findContours(overlapBW,1,2)
+    cnt = contours[0]
+    print(len(contours))
+    area = cv2.contourArea(cnt)
+    
+    h,w,d = bin_img.shape
+    total_area = h*w
+    overlap_ptg = (area/total_area)
+    print("Overlap percentage: ", overlap_ptg)
+    if overlap_ptg < 50:
+        return True
+    else:
+        return False
 
-        p_h = float(patch.shape[0])
-        p_w = float(patch.shape[1])
-        assert p_h == data["imageHeight"]
-        assert p_w == data["imageWidth"]
-
-        dsp_pts = []
-        i=0
-        for shape in range(len(data["shapes"])):
-            for pt in data["shapes"][shape]["points"]:
-                #print(f"Original Point: {pt}")
-                new_pt = [pt[0],pt[1]]
-
-                if ROT:                            #Apply rotation
-                    rot_og = (p_w/2, p_h/2)
-                    new_pt = rotate(new_pt,angle,rot_og)
-
-                    M = cv2.getRotationMatrix2D(rot_og, angle, 1.0)     # AJUSTE...
-                    cos = np.abs(M[0, 0])
-                    sin = np.abs(M[0, 1])
-                    nW = int((p_h * sin) + (p_w * cos))
-                    nH = int((p_h * cos) + (p_w * sin))
-                    new_pt[0] += (nW / 2) - rot_og[0]
-                    new_pt[1] += (nH / 2) - rot_og[1]
-
-                    #print(f"Rotated Point: {new_pt}")
-
-                if SCALE:                       #Apply resize
-                    #print(f"Resize: {rs}")
-                    rs_x = ((p_w*rs)*new_pt[0])/p_w
-                    rs_y = ((p_h*rs)*new_pt[1])/p_h
-                    new_pt = [rs_x,rs_y]
-                    #print(f"Resized Point: {new_pt}")
-
-                if TRANS:                       #Apply Displacement
-                    new_pt = [float(new_pt[0]+x),float(new_pt[1]+y)]
-                    #print(f"Displaced Point: {new_pt}")
-
-                dsp_pts.append(new_pt)
-                i+=1
-
-            new_data,shp = {},{}
-            #print(data)
-            for field in data:
-                #print(field)
-                if field == "shapes":
-                    new_data[field] = data[field]
-                    new_data[field][shape] = data[field][shape]
-                    shp = data[field][shape]
-                    for subfield in data[field][shape]:
-                        #print(subfield)
-                        if subfield == "points":
-                            new_data[field][shape][subfield] = dsp_pts
-                            shp[subfield] = dsp_pts
-                            #print(new_data[field][subfield])
-                        else:
-                            #print(data[field][subfield])
-                            new_data[field][shape] = data[field][shape]
-                            new_data[field][shape][subfield] = data[field][shape][subfield]
-                            shp[subfield] = data[field][shape][subfield]
-                elif field == "imagePath":
-                    f_data = name
-                    new_data[field] = f_data
-                elif field =="imageData":
-                    encoded = base64.b64encode(open(path_to_new_img, "rb").read())
-                    f_data = ""
-                    new_data[field] = encoded.decode("utf-8") 
-                elif field =="imageWidth":
-                    f_data = ""
-                    new_data[field] = new_img.shape[1]
-                elif field =="imageHeight":
-                    f_data = ""
-                    new_data[field] = new_img.shape[0]
-                else:
-                    f_data = data[field]
-                    new_data[field] = f_data
-        
-        json_name = name.replace(".jpg",".json")
-        if not os.path.exists(os.path.join(AUG_PATH,json_name)):
-            json.dump(new_data,open(os.path.join(AUG_PATH,json_name),"w"),indent=2,separators=(", ",": "),sort_keys=False)
-        else:
-            update_data = json.load(open(os.path.join(AUG_PATH,json_name)))
-            update_data["shapes"].append(shp)
-            json.dump(update_data,open(os.path.join(AUG_PATH,json_name),"w"),indent=2,separators=(", ",": "),sort_keys=False)
-        '''
 #------------------------------------------------------------------------------
 def create_dataset(bg_dataset, objects_dataset):
     """
@@ -421,7 +407,7 @@ if __name__ == "__main__":
     MASK_DATASET = "../cleansea_dataset/Dataset/Objetos"
     BG_DATASET = "../cleansea_dataset/Dataset/Backgrounds"
 
-    #create_new_dataset(BG_DATASET,MASK_DATASET)
-    create_dataset(BG_DATASET, MASK_DATASET)
-    checkMasks()
+    create_new_dataset(BG_DATASET,MASK_DATASET)
+    #create_dataset(BG_DATASET, MASK_DATASET)
+    #checkMasks()
 
