85e47180089648e022a2102f9dc52e91d77a21a9
Jose J. Valero-Mas <jose.jvmas@gmail.com>
2022-09-11 12:59:24 +0100

    Adding material-level evaluation

--- train.py
+++ train.py
@@ -24,6 +24,37 @@ from CleanSeaDataset import CleanSeaDataset
 import argument_parsing
 
 
+
+# Dictionary from instance to material level:
+mapping_dictionary = {
+    # bottle, plastic_bag, glove, fishing_net, tire, plastic_debris, pipe -> plastic
+    '4' : 1,
+    '5' : 1,
+    '6' : 1,
+    '7' : 1,
+    '8' : 1,
+    '14' : 1,
+    '16' : 1,
+    # can, squared_can, wasingmachine, metal_chain, metal_debris, car_bumper -> metal
+    '1' : 2,
+    '2' : 2,
+    '10' : 2,
+    '11' : 2,
+    '15' : 2,
+    '18' : 2,
+    # wood -> wood
+    '3' : 3,
+    # packaging_bag -> paper
+    '9' : 4,
+    # rope, towel, shoe, basket -> other
+    '12' : 5,
+    '13' : 5,
+    '17' : 5,
+    '19' : 5,
+}
+
+
+
 """ Train process """
 def train_process(args):
     physical_devices = tf.config.list_physical_devices('GPU')
@@ -177,51 +208,100 @@ def inference_process(args):
         print("\t - Done!")
 
         # Iterating through the different test images:
-        image_ids = dataset_test.image_ids
-        APs_025 = list()
-        APs_05 = list()
-        APs_075 = list()
-        AP_ranges = list()
-        IoUs = list()
-        for image_id in image_ids:
+        APs_025_instance = list()
+        APs_05_instance = list()
+        APs_075_instance = list()
+        AP_ranges_instance = list()
+        IoUs_instance = list()
+        APs_05_material = list()
+        APs_075_material = list()
+        APs_025_material = list()
+        IoUs_material = list()
+        AP_ranges_material = list()
+        for image_id in dataset_test.image_ids:
             # Load image and ground truth data
             image, image_meta, gt_class_id, gt_bbox, gt_mask =\
                 modellib.load_image_gt(dataset_test, inference_config, image_id)
-            # molded_images = np.expand_dims(modellib.mold_image(image, inference_config), 0)
-            
+
+
             # Run object detection
             results = model.detect([image], verbose=0)
             r = results[0]
             
+
+            #### /INSTANCE-LEVEL EVALUATION\ ###
             # Compute AP
             # th = 0.5
             AP_05, precisions, recalls, overlap = utils.compute_ap(gt_bbox, gt_class_id, gt_mask, r["rois"], r["class_ids"], r["scores"], r['masks'], 0.5)
-            APs_05.append(AP_05)
+            APs_05_instance.append(AP_05)
 
             # th = 0.75
             AP_075, precisions, recalls, overlap = utils.compute_ap(gt_bbox, gt_class_id, gt_mask, r["rois"], r["class_ids"], r["scores"], r['masks'], 0.75)
-            APs_075.append(AP_075)
+            APs_075_instance.append(AP_075)
 
             # th = 0.25
             AP_025, precisions, recalls, overlap = utils.compute_ap(gt_bbox, gt_class_id, gt_mask, r["rois"], r["class_ids"], r["scores"], r['masks'], 0.25)
-            APs_025.append(AP_025)
+            APs_025_instance.append(AP_025)
 
             # AVG Intersection over Union:
-            IoUs.append(np.average(overlap))
+            IoUs_instance.append(np.average(overlap))
 
             # COCO range
             AP_range = utils.compute_ap_range(gt_box = gt_bbox, gt_class_id = gt_class_id, gt_mask = gt_mask,\
                      pred_box = r["rois"], pred_class_id = r["class_ids"], pred_score = r["scores"], pred_mask = r['masks'],\
                      iou_thresholds = None, verbose = 0)
-            AP_ranges.append(AP_range)
+            AP_ranges_instance.append(AP_range)
+            #### \INSTANCE-LEVEL EVALUATION/ ###
+
+
+
+            #### /MATERIAL-LEVEL EVALUATION\ ###
+
+            # Grouping classes:
+            gt_class_id_material = np.array([mapping_dictionary[str(u)] for u in gt_class_id], dtype=np.int32)
+            class_ids_material = np.array([mapping_dictionary[str(u)] for u in r["class_ids"]],dtype=np.int32)
+
+            # Compute AP
+            # th = 0.5
+            AP_05, precisions, recalls, overlap = utils.compute_ap(gt_bbox, gt_class_id_material, gt_mask, r["rois"], class_ids_material, r["scores"], r['masks'], 0.5)
+            APs_05_material.append(AP_05)
+
+            # th = 0.75
+            AP_075, precisions, recalls, overlap = utils.compute_ap(gt_bbox, gt_class_id_material, gt_mask, r["rois"], class_ids_material, r["scores"], r['masks'], 0.75)
+            APs_075_material.append(AP_075)
+
+            # th = 0.25
+            AP_025, precisions, recalls, overlap = utils.compute_ap(gt_bbox, gt_class_id_material, gt_mask, r["rois"], class_ids_material, r["scores"], r['masks'], 0.25)
+            APs_025_material.append(AP_025)
+
+            # AVG Intersection over Union:
+            IoUs_material.append(np.average(overlap))
+
+            # COCO range
+            AP_range = utils.compute_ap_range(gt_box = gt_bbox, gt_class_id = gt_class_id_material, gt_mask = gt_mask,\
+                     pred_box = r["rois"], pred_class_id = class_ids_material, pred_score = r["scores"], pred_mask = r['masks'],\
+                     iou_thresholds = None, verbose = 0)
+            AP_ranges_material.append(AP_range)
+            #### \MATERIAL-LEVEL EVALUATION/ ###
+
 
 
         print("--- Results --- ")    
-        print("\t - IoU ({}): {:.2f}".format(MODEL_NAME, np.nanmean(IoUs)))
-        print("\t - mAP-Range ({}): {:.2f}%".format(MODEL_NAME, 100*np.mean(AP_ranges)))
-        print("\t - mAP-0.25 ({}): {:.2f}%".format(MODEL_NAME, 100*np.mean(APs_025)))
-        print("\t - mAP-0.5 ({}): {:.2f}%".format(MODEL_NAME, 100*np.mean(APs_05)))
-        print("\t - mAP-0.75 ({}): {:.2f}%".format(MODEL_NAME, 100*np.mean(APs_075)))
+        print(" - Instance - ")
+        print("\t - IoU ({}): {:.2f}".format(MODEL_NAME, np.nanmean(IoUs_instance)))
+        print("\t - mAP-Range ({}): {:.2f}%".format(MODEL_NAME, 100*np.mean(AP_ranges_instance)))
+        print("\t - mAP-0.25 ({}): {:.2f}%".format(MODEL_NAME, 100*np.mean(APs_025_instance)))
+        print("\t - mAP-0.5 ({}): {:.2f}%".format(MODEL_NAME, 100*np.mean(APs_05_instance)))
+        print("\t - mAP-0.75 ({}): {:.2f}%".format(MODEL_NAME, 100*np.mean(APs_075_instance)))
+
+        print(" - Material - ")
+        print("\t - IoU ({}): {:.2f}".format(MODEL_NAME, np.nanmean(IoUs_material)))
+        print("\t - mAP-Range ({}): {:.2f}%".format(MODEL_NAME, 100*np.mean(AP_ranges_material)))
+        print("\t - mAP-0.25 ({}): {:.2f}%".format(MODEL_NAME, 100*np.mean(APs_025_material)))
+        print("\t - mAP-0.5 ({}): {:.2f}%".format(MODEL_NAME, 100*np.mean(APs_05_material)))
+        print("\t - mAP-0.75 ({}): {:.2f}%".format(MODEL_NAME, 100*np.mean(APs_075_material)))
+
+
 
     return
 
